63f48915208ccca5733f4eac17d2301dbf171582
Abubakar <abubakarkhan909@gmail.com>
2024-07-07 04:32:56 +0500

    component making and sidebar toggle add

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import './Home.css'
+
+const Home = ({ sidebar }) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar}/>
+    </>
+  )
+}
+
+export default Home
